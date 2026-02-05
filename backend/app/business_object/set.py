452c4153706e31f063033353eff8473749750b2b
MarieckeBV <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2026-02-05 11:40:00 +0100

    :necktie: business objects creation

---
+++ backend/app/business_object/set.py
@@ -0,0 +1,7 @@
+class Set:
+    # constructeur
+    def __init__(self, set_num, name, year, id=None):
+        self.set_num = set_num
+        self.id = id
+        self.name = name
+        self.year = year
---
+++ backend/app/business_object/user.py
@@ -0,0 +1,6 @@
+class User:
+    # constructeur
+    def __init__(self, username, password, id=None):
+        self.username = username
+        self.id = id
+        self.password = password
