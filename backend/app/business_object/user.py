e0bbec305251ab5a2e03d645dc3d221c749d41ee
Lucas-Repetti <lucas.repetti--tuya@eleve.ensai.fr>
2026-02-09 15:57:46 +0100

    💩 Business de user
    business de user

--- backend/app/business_object/user.py
+++ backend/app/business_object/user.py
@@ -1,7 +1,29 @@
 class User:
-    # constructeur
-    def __init__(self, username, password, salt=None, id_user=None):
-        self.username = username
+    def __init__(self, username, hashed_password, salt, id_user=None):
         self.id_user = id_user
-        self.password = password
+        self.username = username
+        self.hashed_password = hashed_password
         self.salt = salt
+
+    def __str__(self) -> str:
+        return f"{self.username} (id={self.id_user})"
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id_user=data.get("id_user"),
+            username=data.get("username"),
+            hashed_password=data.get("hashed_password"),
+            salt=data.get("salt")
+        )
+
+    def to_dict(self):
+        """
+        À utiliser UNIQUEMENT pour la persistance
+        """
+        return {
+            "id_user": self.id_user,
+            "username": self.username,
+            "hashed_password": self.hashed_password,
+            "salt": self.salt
+        }
